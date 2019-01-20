cd3777d08911a83d5c8b1a76b198a7a827c96042
Ruijin Wang <cpmunichwrj@gmail.com>
2019-01-20 15:09:36 +0800

    Create 1088A.py

---
+++ Codeforces/1088A.py
@@ -0,0 +1,12 @@
+import math
+x = int(input())
+if x == 1:
+    print(-1)
+elif x == 2:
+    print('2 2')
+elif x == 3:
+    print('2 2')
+else:
+    b = int(math.sqrt(x/2))+1
+    a = 2 * b
+    print(a, b)
